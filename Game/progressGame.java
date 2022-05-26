bb1861401841f9b971595faae20a63d5da2f7958
mictan23 <mictan23@bergen.org>
2022-05-26 10:17:34 -0400

    Create progressGame.java

---
+++ Game/progressGame.java
@@ -0,0 +1,22 @@
+package Game;
+
+public class progressGame {
+    
+    /*
+    if getValue = 11, then
+        j
+    if getValue = 12, then
+        Q
+    if card.getValue().equals(13) {
+        K
+    if car.getValue().equals(1) {
+
+    }
+*/
+
+
+//  boolean validSlap = false;
+//  validSlap: then playerNUM.getCards().
+
+
+}
